92d84f860596a97e280d1592563c5273e3a52a1b
rchekr <rchekr@yandex.ru>
2021-10-03 14:32:47 +0300

    GUI beta 2.0 (optimization)

--- main.py
+++ main.py
@@ -3,50 +3,33 @@ from tkinter import *
 
 
 def proection():
-    label00.config(text=is0(field[0][0]), bg=colorchooser(field[0][0]))
-    label01.config(text=is0(field[0][1]), bg=colorchooser(field[0][1]))
-    label02.config(text=is0(field[0][2]), bg=colorchooser(field[0][2]))
-    label03.config(text=is0(field[0][3]), bg=colorchooser(field[0][3]))
-    label10.config(text=is0(field[1][0]), bg=colorchooser(field[1][0]))
-    label11.config(text=is0(field[1][1]), bg=colorchooser(field[1][1]))
-    label12.config(text=is0(field[1][2]), bg=colorchooser(field[1][2]))
-    label13.config(text=is0(field[1][3]), bg=colorchooser(field[1][3]))
-    label20.config(text=is0(field[2][0]), bg=colorchooser(field[2][0]))
-    label21.config(text=is0(field[2][1]), bg=colorchooser(field[2][1]))
-    label22.config(text=is0(field[2][2]), bg=colorchooser(field[2][2]))
-    label23.config(text=is0(field[2][3]), bg=colorchooser(field[2][3]))
-    label30.config(text=is0(field[3][0]), bg=colorchooser(field[3][0]))
-    label31.config(text=is0(field[3][1]), bg=colorchooser(field[3][1]))
-    label32.config(text=is0(field[3][2]), bg=colorchooser(field[3][2]))
-    label33.config(text=is0(field[3][3]), bg=colorchooser(field[3][3]))
+    for i in range(4):
+        for j in range(4):
+            tiles[i][j].config(text=is0(field[i][j]), bg=colorchooser(field[i][j]))
     label_score.config(text=("Score: "+str(score)))
 
 
 def colorchooser(a):
-    if a == 0:
-        return "#CCC0B3"
-    elif a == 2:
-        return "#EEE4DA"
-    elif a == 4:
-        return "#EDE0C8"
-    elif a == 8:
-        return "#F2B179"
-    elif a == 16:
-        return "#F59563"
-    elif a == 32:
-        return "#F67C5F"
-    elif a == 64:
-        return "#F65E3B"
-    elif a == 128:
-        return "#EDCC72"
-    elif a == 256:
-        return "#EDCF61"
-    elif a == 512:
-        return "#EDC850"
-    elif a == 1024:
-        return "#EDC53F"
-    elif a >= 2048:
-        return "#EDC22E"
+    colors = {
+        0: "#CCC0B3",
+        2: "#EEE4DA",
+        4: "#EDE0C8",
+        8: "#F2B179",
+        16: "#F59563",
+        32: "#F67C5F",
+        64: "#F65E3B",
+        128: "#EDCC72",
+        256: "#EDCF61",
+        512: "#EDC850",
+        1024: "#EDC53F",
+        2048: "#EDC22E",
+    }
+
+    for i in colors.keys():
+        if a > 2048:
+            return colors.get(2048)
+        elif a == i:
+            return colors.get(i)
 
 
 def is0(a):
@@ -157,41 +140,14 @@ window.bind('<Up>', move_up)
 window.bind('<Left>', move_left)
 window.bind('<Down>', move_down)
 window.bind('<Right>', move_right)
-
-label00 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label01 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label02 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label03 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label10 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label11 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label12 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label13 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label20 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label21 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label22 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label23 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label30 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label31 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label32 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
-label33 = Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RIDGE, bd=5)
+tiles = [[Label(window, text='', font=('Arial', 30), height=2, width=4, relief=RAISED, bd=2)
+          for j in range(4)] for k in range(4)]
 label_score = Label(window, text='Score: 0', font=("Arial", 45))
 
-label00.grid(row=0, column=0)
-label01.grid(row=0, column=1)
-label02.grid(row=0, column=2)
-label03.grid(row=0, column=3)
-label10.grid(row=1, column=0)
-label11.grid(row=1, column=1)
-label12.grid(row=1, column=2)
-label13.grid(row=1, column=3)
-label20.grid(row=2, column=0)
-label21.grid(row=2, column=1)
-label22.grid(row=2, column=2)
-label23.grid(row=2, column=3)
-label30.grid(row=3, column=0)
-label31.grid(row=3, column=1)
-label32.grid(row=3, column=2)
-label33.grid(row=3, column=3)
+for i in range(4):
+    for j in range(4):
+        tiles[i][j].grid(row=i, column=j)
+
 label_score.grid(row=4, column=0, columnspan=4)
 
 new_step()
