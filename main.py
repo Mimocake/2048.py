c69ca9fab5acc6fb7ec474589c163da7a0e98e25
rchekr <rchekr@yandex.ru>
2023-05-02 20:15:41 +0300

    2.0 release

--- main.py
+++ main.py
@@ -2,36 +2,6 @@ from tkinter import *
 import random
 
 
-def is0(a):
-    if not a == 0:
-        return str(a)
-    else:
-        return ' '
-
-
-def colorchooser(a):
-    colors = {
-        0: "#CCC0B3",
-        2: "#EEE4DA",
-        4: "#EDE0C8",
-        8: "#F2B179",
-        16: "#F59563",
-        32: "#F67C5F",
-        64: "#F65E3B",
-        128: "#EDCC72",
-        256: "#EDCF61",
-        512: "#EDC850",
-        1024: "#EDC53F",
-        2048: "#EDC22E",
-    }
-
-    for i in colors.keys():
-        if a > 2048:
-            return colors.get(2048)
-        elif a == i:
-            return colors.get(i)
-
-
 class Tile:
 
     def __init__(self, row, column):
@@ -46,22 +16,54 @@ class Screen(object):
         self.screen = Tk()
         self.screen.title("2048")
         self.screen.resizable(False, False)
-        self.tiles = [[Label(self.screen, text='', font=('Arial', 30), height=2, width=4, relief=RAISED, bd=2)
-                    for j in range(size)] for k in range(size)]
+        self.tiles = [[Label(self.screen, text='', font=('Arial', 30),
+                             height=2, width=4, relief=RAISED, bd=2)
+                             for j in range(size)] for k in range(size)]
         for i in range(size):
             for j in range(size):
                 self.tiles[i][j].grid(row=i, column=j)
         self.label_score = Label(self.screen, text='Score: 0', font=("Arial", 40))
         self.label_score.grid(row=size, column=0, columnspan=size)
-        Button(self.screen, text='Restart', font=("Arial", 45), bd=2).grid(row=size+1, column=0, columnspan=size)
-        self.l_lose = Label(self.screen, text="", font=("Arial", 45))
+        Button(self.screen, command=self.restart, text='Restart',
+               font=("Arial", 45), bd=2).grid(row=size+1, column=0, columnspan=size)
+        self.l_lose = Label(self.screen, text="", font=("Clear Sans", 45))
         self.l_lose.grid(row=size, column=0)
 
-    def project(self, brd, score):
+    def project(self):
+        colors = {
+            0: "#CCC0B3",
+            2: "#EEE4DA",
+            4: "#EDE0C8",
+            8: "#F2B179",
+            16: "#F59563",
+            32: "#F67C5F",
+            64: "#F65E3B",
+            128: "#EDCC72",
+            256: "#EDCF61",
+            512: "#EDC850",
+            1024: "#EDC53F",
+            2048: "#EDC22E",
+        }
         for i in range(self.size):
             for j in range(self.size):
-                self.tiles[i][j].config(text=is0(brd[i][j]), bg=colorchooser(brd[i][j]))
-        self.label_score.config(text=("Score: " + str(score)))
+                self.tiles[i][j].config(text=(board.board[i][j] if board.board[i][j] != 0 else ""),
+                                        bg=colors.get(2048) if board.board[i][j] > 2048
+                                        else colors.get(board.board[i][j]))
+        self.label_score.config(text=("Score: " + str(board.score)))
+        self.screen.update()
+
+    def restart(self):
+        board.board = [[0] * size for i in range(size)]
+        board.score = 0
+        board.new_step()
+        board.new_step()
+        self.l_lose = Label(self.screen, text="", font=("Arial", 45))
+        self.l_lose.grid(row=size, column=0)
+        self.project()
+
+    def game_over(self):
+        self.l_lose.grid(row=self.size+2, column=0, columnspan=4)
+        self.l_lose.config(text="You lose!")
         self.screen.update()
 
 
@@ -78,9 +80,10 @@ class Board(object):
             for j in range(0, self.size):
                 if self.board[i][j] == 0:
                     free.append(Tile(i, j))
-        if len(free) == 0:
-            print("Game over")
-            return None
+        for i in self.board:
+            if 0 not in i:
+                if self.game_over():
+                    screen.game_over()
         rtile = random.choice(free)
         self.board[rtile.r][rtile.c] = (random.randint(0, 9) == 9) * 2 + 2
 
@@ -97,62 +100,63 @@ class Board(object):
                 elif direc == 90:
                     self.board[self.size - 1 - j][i] = board2[i][j]
 
-    def compress(self):
-        for k in range(self.size-1):
-            for i in range(self.size-1, k, -1):
-                for j in range(self.size):
-                    if self.board[i-1][j] != 0 and self.board[i][j] == 0:
-                        self.board[i][j] = self.board[i-1][j]
-                        self.board[i-1][j] = 0
+    def move(self):
+        for j in range(4):
+            for n in range(4):
+                for i in range(3, 0, -1):
+                    if self.board[i][j] == 0:
+                        self.board[i - 1][j], self.board[i][j] = 0, self.board[i - 1][j]
+                    elif self.board[i][j] == self.board[i - 1][j]:
+                        self.score += self.board[i][j] * 2
+                        self.board[i - 1][j], self.board[i][j] = 0, self.board[i][j] * 2
 
-    def merge(self):
-        for k in range(self.size-1):
-            for i in range(self.size-1, k, -1):
-                for j in range(self.size):
-                    if self.board[i-1][j] == self.board[i][j]:
-                        self.board[i][j] *= 2
-                        self.score += self.board[i][j]
-                        self.board[i-1][j] = 0
+    def game_over(self):
+        b = True
+        for i in range(4):
+            for j in range(3):
+                if board.board[i][j] == board.board[i][j + 1]:
+                    b = False
+        for j in range(4):
+            for i in range(3):
+                if board.board[i][j] == board.board[i + 1][j]:
+                    b = False
+        return b
 
     def move_up(self, event):
+        board2 = [[i for i in o] for o in board.board]
         self.rotate(90)
         self.rotate(90)
-        self.compress()
-        self.merge()
-        self.compress()
+        self.move()
         self.rotate(90)
         self.rotate(90)
-        self.new_step()
-        screen.project(self.board, self.score)
+        if board2 != board.board:
+            self.new_step()
+        screen.project()
 
     def move_down(self, event):
-        self.compress()
-        self.merge()
-        self.compress()
-        self.new_step()
-        screen.project(self.board, self.score)
+        board2 = [[i for i in o] for o in board.board]
+        self.move()
+        if board2 != board.board:
+            self.new_step()
+        screen.project()
 
     def move_left(self, event):
+        board2 = [[i for i in o] for o in board.board]
         self.rotate(90)
-        self.compress()
-        self.merge()
-        self.compress()
+        self.move()
         self.rotate(-90)
-        self.new_step()
-        screen.project(self.board, self.score)
+        if board2 != board.board:
+            self.new_step()
+        screen.project()
 
     def move_right(self, event):
+        board2 = [[i for i in o] for o in board.board]
         self.rotate(-90)
-        self.compress()
-        self.merge()
-        self.compress()
+        self.move()
         self.rotate(90)
-        self.new_step()
-        screen.project(self.board, self.score)
-
-    def print(self):
-        for i in range(0, self.size):
-            print('|'.join(map(str, self.board[i])))
+        if board2 != board.board:
+            self.new_step()
+        screen.project()
 
 
 size = int(input("Enter board size: "))
@@ -164,5 +168,5 @@ screen.screen.bind('<s>', board.move_down)
 screen.screen.bind('<d>', board.move_right)
 board.new_step()
 board.new_step()
-screen.project(board.board, board.score)
+screen.project()
 screen.screen.mainloop()
